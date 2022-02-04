5332eb3187608b27ec124158dcdee295a47007e1
tommcn <tommcn@mcnamer.ca>
2022-02-04 16:49:50 -0500

    Init devices

--- devices.ts
+++ devices.ts
@@ -1,7 +1,40 @@
 import { readFile } from "fs/promises";
 import YAML from "yaml";
 
+import { StreetLamp, WeatherStation } from "./things";
+
+const interval = 0.1; // seconds
+
+// TODO: set up YAML schema instead
+interface Config {
+    devices: {
+        weather_stations: {
+            name: string;
+        }[];
+        street_lights: {
+            name: string;
+        }[];
+    };
+}
+
 export async function initDevices() {
-    const config = YAML.parse(await readFile("./devices.yml", "utf8"));
-    console.log(config);
+    const config = YAML.parse(
+        await readFile("./devices.yml", "utf8")
+    ) as Config;
+
+    for (const ws of config.devices.weather_stations) {
+        const created = new WeatherStation(ws.name);
+        setInterval(async () => {
+            await created.tick();
+        }, interval * 1000);
+        console.log("Created weather station:", created.name);
+    }
+
+    for (const sl of config.devices.street_lights) {
+        const created = new StreetLamp(sl.name);
+        setInterval(async () => {
+            await created.tick();
+        });
+        console.log("Created street lamp:", created.name);
+    }
 }
--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,8 @@
-import { StreetLamp } from "../things";
-import { WeatherStation } from "../things";
-import { saveDeviceState, setUp } from "./db";
-import { Devices } from "./types";
-
-const devices: Devices = {
-    sls: [new StreetLamp("sl-1"), new StreetLamp("sl-2")],
-    wss: [new WeatherStation("ws-1")],
-};
+import { setUp } from "./db";
 
 export async function startServer(): Promise<void> {
+    // Set up InfluxDB
     await setUp("user", "password", "org", "bucket");
-    setInterval(async () => {
-        const values = await Promise.all([
-            devices.sls.map((sl) => sl.tick()),
-            devices.wss.map((ws) => ws.tick()),
-        ]);
-        saveDeviceState(devices);
-    }, 0.1 * 1000);
+
+    // TODO: Start MQTT
 }
